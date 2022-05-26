ddf6434b22bdaff515e6ce398aa6d61d6b66d680
Bruce Ediger <bediger8@gmail.com>
2022-05-25 18:27:38 -0600

    Ability to call more methods

--- main.go
+++ main.go
@@ -28,6 +28,18 @@ func main() {
 		case "pushL":
 			q = q.PushLeft(os.Args[idx+1])
 			idx += 2
+		case "popR":
+			idx++
+			data, q = q.PopRight()
+			fmt.Printf("pop right: %v\n", data)
+		case "pushR":
+			q = q.PushRight(os.Args[idx+1])
+			idx += 2
+		case "print":
+			q.Print(os.Stdout)
+			idx++
+		default:
+			idx++
 		}
 	}
 }
