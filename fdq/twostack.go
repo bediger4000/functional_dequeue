db69ebd46b12f5babf74d32f8f0a81152a0bf65e
Bruce Ediger <bediger8@gmail.com>
2022-05-26 08:42:27 -0600

    tradition, 2-stack implementation

---
+++ fdq/twostack.go
@@ -0,0 +1,92 @@
+package fdq
+
+import (
+	"fmt"
+	"os"
+)
+
+type stackNode struct {
+	data any
+
+	next *stackNode
+}
+
+type TwoStack struct {
+	head *stackNode
+	tail *stackNode
+}
+
+var _ Dequeue = (*TwoStack)(nil)
+
+func (l *TwoStack) PushLeft(datum any) Dequeue {
+	if l == nil {
+		l = &TwoStack{}
+	}
+	node := &stackNode{data: datum}
+	node.next = l.head
+	l.head = node
+	return l
+}
+func (l *TwoStack) PopLeft() (any, Dequeue) {
+	if l == nil {
+		l = &TwoStack{}
+	}
+	if l.head == nil {
+		for node := l.head; node != nil; node = l.head {
+			l.head = l.head.next
+			node.next = l.tail
+			l.tail = node
+		}
+	}
+	if l.head == nil {
+		return nil, l
+	}
+	node := l.tail
+	l.tail = l.tail.next
+	return node.data, l
+}
+
+func (l *TwoStack) PushRight(datum any) Dequeue {
+	if l == nil {
+		l = &TwoStack{}
+	}
+	node := &stackNode{data: datum}
+	node.next = l.tail
+	l.tail = node
+	return l
+}
+
+func (l *TwoStack) PopRight() (any, Dequeue) {
+	if l == nil {
+		l = &TwoStack{}
+	}
+	if l.tail == nil {
+		for node := l.head; node != nil; node = l.head {
+			node.next = l.tail
+			l.tail = node
+			l.head = l.head.next
+		}
+	}
+	if l.tail == nil {
+		return nil, l
+	}
+	node := l.tail
+	l.tail = l.tail.next
+	return node.data, l
+}
+
+func (l *TwoStack) Print(fout *os.File) {
+	if l == nil {
+		l = &TwoStack{}
+	}
+	fmt.Fprintf(fout, "head: ")
+	for p := l.head; p != nil; p = p.next {
+		fmt.Fprintf(fout, "%s -> ", p.data)
+	}
+	fmt.Fprintf(fout, "\n")
+	fmt.Fprintf(fout, "tail: ")
+	for p := l.tail; p != nil; p = p.next {
+		fmt.Fprintf(fout, "%s -> ", p.data)
+	}
+	fmt.Fprintf(fout, "\n")
+}
