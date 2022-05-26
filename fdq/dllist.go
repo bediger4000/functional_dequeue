c63a26deffb6b52cd54b153fe9fc10fbe954a26c
Bruce Ediger <bediger8@gmail.com>
2022-05-25 18:26:39 -0600

    Fill in PushR, PopR, add Print method

--- fdq/dllist.go
+++ fdq/dllist.go
@@ -1,5 +1,10 @@
 package fdq
 
+import (
+	"fmt"
+	"os"
+)
+
 type listNode struct {
 	data any
 
@@ -12,11 +17,11 @@ type ListQueue struct {
 	tail *listNode
 }
 
+var _ Dequeue = (*ListQueue)(nil)
+
 func NewDllist() Dequeue {
 	return &ListQueue{}
 }
-
-//var _ Dequeque = (*ListQueue)(nil)
 
 func (l *ListQueue) PushLeft(datum any) Dequeue {
 	node := &listNode{data: datum}
@@ -37,15 +42,51 @@ func (l *ListQueue) PopLeft() (any, Dequeue) {
 
 	node := l.head
 	l.head = l.head.next
-	l.head.prev = nil
+	if l.head != nil {
+		l.head.prev = nil
+	}
 
 	return node.data, l
 }
 
 func (l *ListQueue) PushRight(datum any) Dequeue {
+	node := &listNode{data: datum}
+	if l.tail == nil {
+		l.head = node
+		l.tail = node
+		return l
+	}
+	node.prev = l.tail
+	l.tail.next = node
+	l.tail = node
 	return l
 }
 
 func (l *ListQueue) PopRight() (any, Dequeue) {
-	return nil, l
+	if l.tail == nil {
+		return nil, l
+	}
+
+	node := l.tail
+	l.tail = l.tail.prev
+	if l.tail != nil {
+		l.tail.next = nil
+	} else {
+		l.head = nil
+	}
+
+	return node.data, l
+}
+
+func (l *ListQueue) Print(fout *os.File) {
+	fmt.Fprintf(fout, "Left to right: ")
+	for p := l.head; p != nil; p = p.next {
+		fmt.Fprintf(fout, "%s -> ", p.data)
+	}
+	fmt.Fprintf(fout, "\n")
+	fmt.Fprintf(fout, "Right to left: ")
+	for p := l.tail; p != nil; p = p.prev {
+		fmt.Fprintf(fout, "%s -> ", p.data)
+	}
+	fmt.Fprintf(fout, "\n")
 }
