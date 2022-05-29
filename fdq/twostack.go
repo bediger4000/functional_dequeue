2d3978c7dbebdf0407856fabf172ed0a27a010b1
Bruce Ediger <bediger8@gmail.com>
2022-05-28 21:02:53 -0600

    move type stack to its own file

--- fdq/twostack.go
+++ fdq/twostack.go
@@ -5,17 +5,6 @@ import (
 	"os"
 )
 
-type stackNode struct {
-	data any
-	next *stackNode
-}
-
-type stack struct {
-	stk     *stackNode
-	size    int
-	opCount int
-}
-
 type TwoStack struct {
 	opCount int
 	head    *stack
@@ -109,53 +98,4 @@ func (l *TwoStack) Type() string {
 
 func (l *TwoStack) Operations() (int, int) {
 	return l.opCount, l.tail.Operations() + l.head.Operations()
-}
-
-func (s *stack) Operations() int {
-	if s == nil {
-		return 0
-	}
-	return s.opCount
-}
-
-func (s *stack) Node() *stackNode {
-	if s == nil {
-		return nil
-	}
-	return s.stk
-}
-
-func (s *stack) Push(data any) *stack {
-	if s == nil {
-		s = &stack{}
-	}
-	s.opCount++
-	node := &stackNode{data: data}
-	node.next = s.stk
-	s.stk = node
-	s.size++
-	return s
-}
-
-func (s *stack) Pop() (any, *stack) {
-	if s == nil {
-		s = &stack{}
-	}
-	s.opCount++
-
-	if s.stk == nil {
-		return nil, s
-	}
-
-	node := s.stk
-	s.stk = s.stk.next
-	s.size--
-	return node.data, s
-}
-
-func (s *stack) Size() int {
-	if s == nil {
-		return 0
-	}
-	return s.size
 }
